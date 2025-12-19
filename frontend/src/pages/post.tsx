7a62a7e298ae3aaa1a081be4c5498444cd424601
Arnaud Mercier <arnaud.mercier.59@gmail.com>
2025-12-19 16:33:03 +0100

    feat: add routes and layout

---
+++ frontend/src/pages/post.tsx
@@ -0,0 +1,8 @@
+export const Post = () => {
+    return (
+        <div>
+            <h2>Post Page</h2>
+            <p>This is where the post content will go.</p>
+        </div>
+    )
+}
---
+++ frontend/src/pages/registration.tsx
@@ -0,0 +1,8 @@
+export const Registration = () => {
+    return (
+        <div>
+            <h2>Registration Page</h2>
+            <p>This is where the registration form will go.</p>
+        </div>
+    )
+}
