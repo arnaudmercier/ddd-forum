cd2548d5f09da58ae8b5cc4d442a6c3ae489e765
Arnaud Mercier <arnaud.mercier@lyreco.com>
2025-12-22 13:13:44 +0100

    feat(register): add registration form with basic validation

--- frontend/src/pages/registration.tsx
+++ frontend/src/pages/registration.tsx
@@ -1,12 +1,53 @@
 import {Layout} from "./layout"
+import toast, {Toaster} from "react-hot-toast";
 
 export const Registration = () => {
+    function register(formData: FormData) {
+        const email = formData.get("email") as string
+        const username = formData.get("username") as string
+        const firstName = formData.get("firstName") as string
+        const lastName = formData.get("lastName") as string
+
+        if (!email || !username || !firstName || !lastName) {
+            toast.error('Missing required fields', {'position': 'top-right'});
+            return
+        }
+
+        console.log("Receive registration request for:" + {email, username, firstName, lastName})
+    }
+
     return (
         <Layout>
-            <div>
-                <h2>Registration Page</h2>
-                <p>This is where the registration form will go.</p>
-            </div>
+            <Toaster/>
+            <form action={register}>
+                <div className="registration-form">
+                    <div>Create Account</div>
+                    <input className="registration email"
+                           type="email" placeholder="email"
+                           name="email"/>
+                    <input
+                        className="registatation-input username"
+                        type="text"
+                        placeholder="username"
+                        name="username"
+                    />
+                    <input
+                        className="registatation-input username"
+                        type="text"
+                        placeholder="first name"
+                        name="firstName"
+                    />
+                    <input
+                        className="registatation-input username"
+                        type="text"
+                        placeholder="last name"
+                        name="lastName"
+                    />
+                    <div>
+                        <button className="submit-button" type="submit">Submit</button>
+                    </div>
+                </div>
+            </form>
         </Layout>
     )
 }
