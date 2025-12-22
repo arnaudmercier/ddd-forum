7e6d9079c66f96e6af7fdb8c10f5760d738eb9cd
Arnaud Mercier <arnaud.mercier@lyreco.com>
2025-12-22 15:52:21 +0100

    chore: add cors headers

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,4 @@
-import express, {Request, Response} from "express";
+import express from "express";
 import {createUserRouter} from "./user-http";
 import {Config} from "./config";
 import cors from 'cors';
