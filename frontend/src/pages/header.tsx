1ed2a225c3b9676b7dfd2b628dca00d70d32526a
Arnaud Mercier <arnaud.mercier@lyreco.com>
2025-12-23 11:45:36 +0100

    feat(register): use context

--- frontend/src/pages/header.tsx
+++ frontend/src/pages/header.tsx
@@ -1,18 +1,37 @@
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
+import AuthContext from "../authContext.ts";
+import {useContext} from "react";
 
 export const Header = () => {
+
+    const {username} = useContext(AuthContext);
+
     return (
         <header id="header" className="flex align-center">
             <div id="app-logo">
-                <img src="../assets/dddforumlogo.png"/>
+                <img src="/assets/dddforumlogo.png" alt="DDD Forum Logo"/>
             </div>
             <div id="title-container">
                 <h1>Domain-Driven Designers</h1>
                 <h3>Where awesome domain driven designers are made</h3>
                 <a href="/submit">submit</a>
             </div>
-            <div id="header-action-button">
-                <Link to="/register">Join</Link>
+            <div className="header-actions-container">
+                {!username && (
+                    <div className="header-action-button">
+                        <Link to="/register">Join</Link>
+                    </div>
+                )}
+                {username && (
+                    <>
+                        <div className="header-login-name">
+                            {username}
+                        </div>
+                        <div className="header-action-button">
+                            <Link to="/logout">Logout</Link>
+                        </div>
+                    </>
+                )}
             </div>
         </header>
     )
