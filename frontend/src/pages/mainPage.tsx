79b0db8a98be40ad33f050428099cbca23516406
Arnaud Mercier <arnaud.mercier.59@gmail.com>
2025-12-23 16:49:36 +0100

    feat(homepage): display hard coded posts

--- frontend/src/pages/mainPage.tsx
+++ frontend/src/pages/mainPage.tsx
@@ -1,9 +1,27 @@
 import {Layout} from "./layout.tsx";
+import {PostList} from "./postList.tsx";
+import type {Post} from "./post.ts";
 
 export const MainPage = () => {
+    const post1: Post = {
+        id: 1,
+        title: "First Post",
+        content: "This is the content of the first post.",
+        memberId: 1,
+        createdAt: "2025-12-01T12:00:00Z"
+    };
+    const post2: Post = {
+        id: 2,
+        title: "Second Post",
+        content: "This is the content of the second post.",
+        memberId: 1,
+        createdAt: "2025-07-01T12:00:00Z"
+    };
+    const posts: Post[] = [post1, post2];
+
     return (
         <Layout>
-            <div className="content">post</div>
+            <PostList posts={posts}/>
         </Layout>
     )
 }
